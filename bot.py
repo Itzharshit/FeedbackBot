5cd332e2e699e99e0e3bd52598cf246a96ec4b7c
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-11-18 15:50:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,7 +85,85 @@ async def pin(bot, message):
     else:
          await message.reply_text("<b>That's not for you bruh 😅</b>")
                                     
-
+@Bot.on_inline_query()
+async def inline_handlers(_, event: InlineQuery):
+    answers = list()
+    # If Search Query is Empty
+    if event.query == "":
+        answers.append(
+            InlineQueryResultArticle(
+                title="Tutorial Video",
+                description="If you are facing any problem In opening Tnlink, Watch this Tutorial...",
+                thumb_url="https://i.imgur.com/6jZsMYG.png",
+                input_message_content=InputTextMessageContent(
+                    message_text="Please watch this video if you are facing problem in opening links.",
+                    disable_web_page_preview=True
+                ),
+                reply_markup=InlineKeyboardMarkup([
+                    [InlineKeyboardButton("Watch Tutorial", url="https://jmp.sh/q24v5ga")]
+                ])
+            )
+        )
+        answers.append(
+            InlineQueryResultArticle(
+                title="Support Channel & Group",
+                description="Channel - @pocketfmhub\nGroup - @pocketfmhubchat",
+                thumb_url="https://i.ibb.co/cNYJHYZ/IMG-20210815-144921.jpg",
+                input_message_content=InputTextMessageContent(
+                    message_text="Using this bot you can search all the available audiobooks of pocket Fm Hub without visiting main channel",
+                    disable_web_page_preview=True
+                ),
+                reply_markup=InlineKeyboardMarkup([
+                    [InlineKeyboardButton("Support Group", url="https://t.me/pocketfmhubchat"),
+                     InlineKeyboardButton("Bots Channel", url="https://t.me/pocketfmhub")],
+                    [InlineKeyboardButton("Search Here", switch_inline_query_current_chat="")]
+                ])
+            )
+        )
+        try:
+            await event.answer(
+            results=answers,
+            cache_time=0
+            )
+            print(f"[{Config.BOT_SESSION_NAME}] - Answered Successfully - {event.from_user.first_name}")
+        except QueryIdInvalid:
+            print(f"[{Config.BOT_SESSION_NAME}] - Failed to Answer - {event.from_user.first_name}")
+    # Search Channel Message using Search Query Words
+    else:
+        txt="All results\n\n"
+        async for message in User.search_messages(chat_id=Config.CHANNEL_ID, limit=49, query=event.query):
+            if message.text:
+                answers.append(InlineQueryResultArticle(
+                    title="{}".format(message.text.split("\n", 1)[0]),
+                    description="{}".format(message.text.rsplit("\n", 1)[-1]),
+                    thumb_url="https://i.ibb.co/BwYhZTr/IMG-20210914-015740-238.jpg",
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Search Again", switch_inline_query_current_chat="")]]),
+                    input_message_content=InputTextMessageContent(
+                        message_text=message.text.markdown,
+parse_mode="markdown",
+                        disable_web_page_preview=True
+                    )
+                ))
+                txt=txt+"{} - https://t.me/pocketfmhub/{}".format(message.text.split("\n", 1)[0],message.message_id)+"\n\n"
+        answers.append(InlineQueryResultArticle(
+                    title="All Episodes",
+                    description="All Episodes",
+                    thumb_url="https://i.ibb.co/4dPd52s/Png-Item-5099442-1.png",
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Search Again", switch_inline_query_current_chat="")]]),
+                    input_message_content=InputTextMessageContent(
+                        message_text=txt,
+                        parse_mode="markdown",
+                        disable_web_page_preview=True
+                    )
+                ))
+        try:
+            await event.answer(
+            results=answers,
+            cache_time=0
+            )
+            print(f"[{Config.BOT_SESSION_NAME}] - Answered Successfully - {event.from_user.first_name}")
+        except QueryIdInvalid:
+            print(f"[{Config.BOT_SESSION_NAME}] - Failed to Answer - {event.from_user.first_name}")
 
 @Bot.on_message(filters.command('id') & filters.group)
 async def id(bot, message):
